35230cdffdc4d287ab2f4ac8e37c27f327c96594
Siddarth Vijay <vsiddarthvijay@gmail.com>
2019-08-19 01:39:16 +0530

    Fixed JSON parser

--- script.py
+++ script.py
@@ -9,13 +9,24 @@ inputFile = open(r"input.json", "r")
 jsonObject = inputFile.read()
 parsedJSON = json.loads(jsonObject)
 
-# Output objects
+# Input objects
+inputStates = parsedJSON["states"]
+inputAlphabet = parsedJSON["letters"]
+inputStart = parsedJSON["start"]
+inputFinal = parsedJSON["final"]
+inputDelta = parsedJSON["t_func"]
 
+# Output objects
+outputStates = parsedJSON["states"]
+outputAlphabet = parsedJSON["letters"]
+outputStart = parsedJSON["start"]
+outputFinal = parsedJSON["final"]
+outputDelta = parsedJSON["t_func"]
 
 # Find all states one by one
 
 
 # Outputing the answer in JSON
-outputFile = open(r"output.json", 'w')
-dfaJSON = json.dumps(dfaDesc)
-print(dfaJSON, file=outputFile)
+# outputFile = open(r"output.json", 'w')
+# dfaJSON = json.dumps(dfaDesc)
+# print(dfaJSON, file=outputFile)
