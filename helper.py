44a7d0b8df4cd2e2ada30749760c63b205c5351b
Siddarth Vijay <vsiddarthvijay@gmail.com>
2019-08-22 22:27:45 +0530

    Made some more additions

--- helper.py
+++ helper.py
@@ -1,5 +1,31 @@
-def phiEntry(state, delta, outputStates):
-    return
+def multiOutputStates(inputAlphabet, delta, inputDelta, outputDelta):
+    currentFinalStates = delta[-1]
+    newSmallFinalStates = []
+    # The old final state will be my new input state
+    for alphabet in inputAlphabet:
+        singleDeltaEntry = []
+        singleDeltaEntry.append(currentFinalStates)
+        singleDeltaEntry.append(alphabet)
+        newSmallFinalStates = []
+        if type(currentFinalStates) != int:
+            for newSmallInputState in currentFinalStates:
+                for processingDelta in inputDelta:
+                    if newSmallInputState in processingDelta:
+                        if alphabet in processingDelta:
+                            newSmallFinalStates.append(processingDelta[-1][0])
+            singleDeltaEntry.append(newSmallFinalStates)
+            outputDelta.append(singleDeltaEntry)
 
-def multiStates():
+            if len(newSmallFinalStates) > 1:
+                print(newSmallFinalStates)
+            else:
+                # return 0, 0
+                print()
+        else:
+            singleDeltaEntry.append(currentFinalStates)
+            outputDelta.append(singleDeltaEntry)
+            return 0, 0
+
+
+def phiEntry(state, delta, outputStates):
     return
--- script.py
+++ script.py
@@ -2,11 +2,14 @@
 import json
 
 # Custom function imports
-from helper import phiEntry, multiStates
+from helper import multiOutputStates, phiEntry
 
 # Reading and processing inputs
-inputFile = open(r"input.json", "r")
+inputFile = open(r"input2.json", "r")
 jsonObject = inputFile.read()
+
+inputFile.close()
+
 parsedJSON = json.loads(jsonObject)
 
 # Input objects
@@ -23,21 +26,30 @@ outputStart = inputStart
 outputFinal = []
 outputDelta = []
 
+# Required variables
+processedStates = []
+
 # Find all states one by one
 for state in range(inputStates):
-    for delta in inputDelta:
-        if state in delta:
-            # print(state, delta)
-            for alphabet in inputAlphabet:
-                if alphabet in delta:
-                    currentFinalStates = delta[-1]
-                    if len(currentFinalStates) == 1:
-                        outputDelta.append(delta)
-                    else:
-                        outputStates += 1
-                        multiOutputs()
+    if state not in processedStates:
+        for delta in inputDelta:
+            if state in delta:
+                for alphabet in inputAlphabet:
+                    if alphabet in delta:
+                        currentFinalStates = delta[-1]
+                        if len(currentFinalStates) == 1:
+                            outputDelta.append(delta)
+                        else:
+                            outputStates += 1
+                            outputDelta.append(delta)
+                            isThereMore = multiOutputStates(inputAlphabet, delta, inputDelta, outputDelta)
+                            if isThereMore[0] == 1:
+                                multiOutputStates(inputAlphabet, isThereMore[1], inputDelta, outputDelta)
 
 # Outputing the answer in JSON
 # outputFile = open(r"output.json", 'w')
 # dfaJSON = json.dumps(dfaDesc)
 # print(dfaJSON, file=outputFile)
+
+# outputFile.close()
+print(outputDelta)
